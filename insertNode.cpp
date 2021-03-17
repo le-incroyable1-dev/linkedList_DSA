43a0f663cdabaf6e510767936a4b6718457597b8
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2021-03-17 20:01:39 +0530

    Update insertNode.cpp

--- insertNode.cpp
+++ insertNode.cpp
@@ -1,3 +1,65 @@
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+
+//RECURSIVE
+
+
+Node* insertNode(Node *head, int i, int data) {
+	// Write your code here
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        return head;
+    }
+    else
+    {
+        if (i < 0)
+            return head;
+        else if (i == 0)
+        {
+            Node *newNode = new Node(data);
+            newNode -> next = head;
+            head = newNode;
+            return head;
+        }
+        else if (i == 1)
+        {
+            Node *newNode = new Node(data);
+            Node *temp = head -> next;
+            head -> next = newNode;
+            newNode -> next = temp;
+            return head;
+        }
+        else
+        {
+            Node *nxt = head -> next;
+            Node *tempDo = insertNode(nxt, i-1, data);
+            return head; 
+        }
+    }
+}
+
+
+
+//ITERATIVE
+
 Node* insertNode (Node *head, int i, int data)
 {
   if (head == NULL)
@@ -31,3 +93,5 @@ Node* insertNode (Node *head, int i, int data)
   }
 
 }
+
+
