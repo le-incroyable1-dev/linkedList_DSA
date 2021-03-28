8285e10454bacf104743280de424dea3c5d2538f
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2021-03-28 06:39:42 +0530

    Create reverseLL.cpp

---
+++ reverseLL.cpp
@@ -0,0 +1,37 @@
+//RECURSIVE ALGORITHM
+
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+ 
+
+Node *reverseLinkedListRec(Node *head)
+{
+    //Write your code here
+    if(head == NULL || head -> next == NULL)
+        return head;
+    else
+    {
+        Node *nxt = head -> next;
+        Node *newHead  = reverseLinkedListRec(nxt);
+        
+        nxt -> next = head;
+        head -> next = NULL;
+        return newHead;
+    }
+    
+}
